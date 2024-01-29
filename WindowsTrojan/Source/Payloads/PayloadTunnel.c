0613d0022ce38921391d8a0c2d751d37790bd563
An-random-creator <158004969+An-random-creator@users.noreply.github.com>
2024-01-28 19:08:50 -0700

    Update PayloadTunnel.c

--- WindowsTrojan/Source/Payloads/PayloadTunnel.c
+++ WindowsTrojan/Source/Payloads/PayloadTunnel.c
@@ -3,7 +3,7 @@
 PAYLOADFUNCTIONVISUAL(payloadTunnel) {
 	PAYLOADHEAD
 
-	StretchBlt(hdc, 50, 50, w - 100, h - 100, hdc, 0, 0, w, h, SRCCOPY);
+	StretchBlt(hdc, 50, 50, w - 10, h - 10, hdc, 0, 0, w, h, SRCCOPY);
 
-	out: return 200.0 / (times / 5.0 + 1) + 4;
+	out: return 200.0 / (times / 1.0 + 1) + 4;
 }
